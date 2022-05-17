3481ae6e1261ec0ea72a88332c8bb93a541593f2
parkinhwa <inhwa6548@naver.com>
2022-05-18 00:00:38 +0900

    fix: functions/movie

--- functions/movie.js
+++ functions/movie.js
@@ -10,15 +10,15 @@ exports.handler = async function (event) {
     : `https://www.omdbapi.com?apikey=${API_KEY}&i=${id}&plot=full`;
   try {
     const { data } = await axios.get(url);
-    if (data.Error) {
-      return {
-        statusCode: 400,
-        body: data.Error,
-      };
-    }
+    // if (data.Error) {
+    //   return {
+    //     statusCode: 400,
+    //     body: data.Error,
+    //   };
+    // }
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: data,
     };
   } catch (error) {
     return { statusCode: error.response.status, body: error.message };
