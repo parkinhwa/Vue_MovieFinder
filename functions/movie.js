ca01e08544ed8bcec9ce3cf0f80abef1e1428d3e
parkinhwa <inhwa6548@naver.com>
2022-05-17 17:39:56 +0900

    fix: functions/movie 수정

--- functions/movie.js
+++ functions/movie.js
@@ -3,12 +3,11 @@ const axios = require('axios')
 const { API_ENDPOINT, API_KEY } = process.env
 
 exports.handler = async function () {
-  const url = await axios({
+  const { url } = await axios({
     url: `${API_ENDPOINT}?apikey=${API_KEY}`,
   })
-  // /.netlify/functions/workspace
   return {
     statusCode: 200,
-    url
+    body: JSON.stringify(url)
   }
 }
