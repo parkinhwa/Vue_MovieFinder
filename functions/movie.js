3163f3edc11997fa6e5b5657f21bb94c8a918616
parkinhwa <inhwa6548@naver.com>
2022-05-17 18:13:12 +0900

    test: functions test..

--- functions/movie.js
+++ functions/movie.js
@@ -3,12 +3,11 @@ const axios = require('axios')
 const { API_ENDPOINT, API_KEY } = process.env
 
 exports.handler = async function () {
-  const { url } = await axios({
-    url: `${API_ENDPOINT}?apikey=${API_KEY}`,
-  })
-  console.log(JSON.stringify(url));
+  const url = `${API_ENDPOINT}?apikey=${API_KEY}`
+  const res = await axios.get(url)
+  console.log(res.data);
   return {
     statusCode: 200,
-    body: JSON.stringify(url)
+    body: JSON.stringify(res.data)
   }
 }
