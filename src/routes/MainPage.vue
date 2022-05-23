da037e89318f83fa94a4a381e21989687ab48327
parkinhwa <inhwa6548@naver.com>
2022-05-24 02:48:57 +0900

    refactory: form 제거, KeyupHandler 오타 수정, 변수명 수정

--- src/routes/MainPage.vue
+++ src/routes/MainPage.vue
@@ -13,7 +13,7 @@
       <div class="container-fluid px-4 px-lg-0">
         <h1 class="fst-italic lh-1 mb-4">Movie Finder</h1>
         <p class="mb-5">Movie Finder에서 좋아하는 영화를 찾아봅시다!</p>
-        <form id="contactForm">
+        <div id="contactForm">
           <div class="row input-group-newsletter">
             <div class="col">
               <input
@@ -35,7 +35,7 @@
               </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
@@ -64,9 +64,9 @@ export default {
         this.SearchPagerouter(this.title);
       }
     },
-    SearchPagerouter(titleinit) {
-      if (titleinit.trim()) {
-        this.$router.push({ name: "SearchPage", params: { titleinit } });
+    SearchPagerouter(titleInit) {
+      if (titleInit.trim()) {
+        this.$router.push({ name: "SearchPage", params: { titleInit } });
       } else {
         alert("영화 제목을 입력해주세요!");
       }
